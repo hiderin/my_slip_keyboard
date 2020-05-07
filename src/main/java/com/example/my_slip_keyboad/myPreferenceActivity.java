528084e48fe7f93318fe3d6c564a70120af1bb7a
Nao By Dyna <naojupier1@gmail.com>
2020-05-07 09:28:13 +0900

    LayoutInflarerをコメントアウト

---
+++ src/main/java/com/example/my_slip_keyboad/myPreferenceActivity.java
@@ -0,0 +1,19 @@
+package com.example.my_slip_keyboad;
+
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+//package com.example.my_slip_keyboad.R;
+
+/**
+ * preference activity
+ */
+public class myPreferenceActivity extends AppCompatActivity implements myPreferenceFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_view);
+    }
+}
---
+++ src/main/java/com/example/my_slip_keyboad/myPreferenceFragment.java
@@ -0,0 +1,61 @@
+package com.example.my_slip_keyboad;
+
+import android.content.Context;
+//import android.graphics.Color;
+//import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+//import android.support.v7.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceFragmentCompat;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+
+//package com.example.my_slip_keyboad.R;
+
+/**
+ * preference fragment
+ */
+public class myPreferenceFragment extends PreferenceFragmentCompat {
+
+    private OnFragmentInteractionListener mListener;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Load the preferences from an XML resource
+        setPreferencesFromResource(R.xml.ime_preferences, rootKey);
+    }
+
+	/*
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        // delete under line
+        setDivider(new ColorDrawable(Color.TRANSPARENT));
+        setDividerHeight(0);
+
+        return view;
+    }
+	*/
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+    }
+}
