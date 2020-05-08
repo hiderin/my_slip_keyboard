ee464aad961022cfe0ec79be22a787965a28d60f
Nao By Dyna <naojupier1@gmail.com>
2020-05-08 09:54:12 +0900

    アプリ名をmy_slip_keyboardに変更

--- src/main/java/com/example/my_slip_keyboard/NewKeyboard.java
+++ src/main/java/com/example/my_slip_keyboard/NewKeyboard.java
@@ -1,4 +1,4 @@
-package com.example.my_slip_keyboad;
+package com.example.my_slip_keyboard;
 
 import android.os.Bundle;
 import android.preference.PreferenceManager;
--- src/main/java/com/example/my_slip_keyboard/myPreferenceFragment.java
+++ src/main/java/com/example/my_slip_keyboard/myPreferenceFragment.java
@@ -1,4 +1,4 @@
-package com.example.my_slip_keyboad;
+package com.example.my_slip_keyboard;
 
 import android.content.Context;
 import android.os.Bundle;
