bcf1ae37bafcc6cf95d653571fe86b4a5d3e13aa
Nao By Dyna <naojupier1@gmail.com>
2020-07-06 15:10:18 +0900

    getLengthListを作成

--- src/main/java/com/example/my_slip_keyboard/Slip2RhText.java
+++ src/main/java/com/example/my_slip_keyboard/Slip2RhText.java
@@ -15,9 +15,11 @@ public class Slip2RhText {
 	private SQLiteDatabase mydb;
 	private Context mContext;
 	private final String dbname = "mydict.db";
+	private final int MOVE_DATA = 0;
+	private final int CHAR_DATA = 1;
 
 	// main text data
-	private RhText mRhText;
+	private ArrayList<RhText> mRhTextList;
 	private StringBuilder mSlipText;
 
 	// constractor
@@ -25,8 +27,37 @@ public class Slip2RhText {
 		mContext = context;
 		hlpr = new LocusSQLiteOpenHelper(context,dbname);
 
-		mRText = new mRhText(context);
+		mRTextList = new ArrayList<mRhTextList>();
 		mSlipText = new StringBuilder();
+	}
+
+	// private method
+	private int getHeadChar2Ascii(String sliptxt){
+		return (int)(sliptxt.toCharArray()[0]);
+	}
+
+	private ArrayList<int> getLengthList(String sliptxt, int idb){
+		ArrayList<int> rtn = new ArrayList<int>();
+		String sqltxt, stable;
+		int ihcode;
+
+		stable = idb == CHAR_DATA ? "slip_char_table" : "move_char_table";
+		ihcode = getHeadChar2Ascii(sliptxt);
+		sqltxt = "SELECT DISTINCT str_len FROM " + stable + " WHERE head_code = ";
+		sqltxt += Integer.toString(ihcode) + " ;";
+
+		mydb = hlpr.getWritableDatabase();
+		SQLiteCursor c = (SQLiteCursor)mydb.rawQuery(sqltxt,null);
+		while(c.moveToNext()){
+			StringBuilder ilist = new StringBuilder(c.getString(0));
+			rtn.add(c.getInt(0));
+		}
+		if(mydb != null) mydb.close();
+
+		return rtn;
+	}
+
+	private ArrayList<RhText> makeRhTextList(String sliptxt){
 	}
 
 	// public method
