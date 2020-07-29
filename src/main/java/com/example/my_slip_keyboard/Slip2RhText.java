b7410a837c8c444c32a32fff3a5844c5c865c14c
Nao By Dyna <naojupier1@gmail.com>
2020-07-29 15:53:55 +0900

    sliptextからRomaTextのﾘｽﾄを再帰的に作成するmakeRomaTextListを作成

--- src/main/java/com/example/my_slip_keyboard/Slip2RhText.java
+++ src/main/java/com/example/my_slip_keyboard/Slip2RhText.java
@@ -35,11 +35,15 @@ public class Slip2RhText {
 		mSlipText = new StringBuilder();
 	}
 
+	////////////////////////////////////////////////////////////////////////////////
 	// private method
+
+	// sliptextの先頭の文字を文字コードに変換
 	private int getHeadChar2Ascii(String sliptxt){
 		return (int)(sliptxt.toCharArray()[0]);
 	}
 
+	// sliptextの頭文字からDBに該当する文字列長さのリストを作成
 	private ArrayList<int> getLengthList(String sliptxt, int idb){
 		ArrayList<int> rtn = new ArrayList<int>();
 		String sqltxt, stable;
@@ -48,12 +52,11 @@ public class Slip2RhText {
 		stable = idb == CHAR_DATA ? "slip_char_table" : "move_char_table";
 		ihcode = getHeadChar2Ascii(sliptxt);
 		sqltxt = "SELECT DISTINCT str_len FROM " + stable + " WHERE head_code = ";
-		sqltxt += Integer.toString(ihcode) + " ;";
+		sqltxt += Integer.toString(ihcode) + " ORDER BY str_len DESC;";
 
 		mydb = hlpr.getWritableDatabase();
 		SQLiteCursor c = (SQLiteCursor)mydb.rawQuery(sqltxt,null);
 		while(c.moveToNext()){
-			StringBuilder ilist = new StringBuilder(c.getString(0));
 			rtn.add(c.getInt(0));
 		}
 		if(mydb != null) mydb.close();
@@ -61,9 +64,59 @@ public class Slip2RhText {
 		return rtn;
 	}
 
-	private ArrayList<RhText> makeRhTextList(String sliptxt){
+	// sliptextから再帰的にRomaTextを作成
+	private ArrayList<String> makeRomaTextList(String sliptxt, int idb){
+		int iLenN, i, j, k;
+		ArrayList<int> iLenList = new ArrayList<int>();
+		String sqltxt, stable, basetext, resttext;
+		ArrayList<String> rtn = new ArrayList<String>();
+
+		stable = idb == CHAR_DATA ? "slip_char_table" : "move_char_table";
+		// ループの準備
+		iLenList = getLengthList(sliptxt, idb);
+		iLenN = iLenList.size();
+
+		// 再帰的処理
+		for(i=0;i<iLenN;i++){
+			if(iLenList(i) > sliptxt.length()) continue;
+			// リストの生成
+			ArrayList<String> baseStrList = new ArrayList<String>();
+			ArrayList<String> restStrLisr = new ArrayList<String>();
+			// sliptxtの分割
+			basetext = sliptxt.substring(0, iLenList(i));
+			resttext = sliptxt.supstring(iLenList(i) - 1, sliptxt.length());
+			// baseStrListの取得
+			if(idb==CHAR_DATA){
+				sqltxt = "SELECT B.roma FROM (SELECT char_no FROM slip_char_table ";
+				sqltxt += "WHERE locus_string = '" + basetext + "') AS A ";
+				sqltxt += "LEFT JOIN (SELECT char_no, roma FROM hira_master_table) AS B ";
+				sqltxt += "ON A.char_no = B.char_no; ";
+				mydb = hlpr.getWritableDatabase();
+				SQLiteCursor c = (SQLiteCursor)mydb.rawQuery(sqltxt,null);
+				while(c.moveToNext()){
+					baseStrList.add(c.getString(0));
+				}
+				if(mydb != null) mydb.close();
+			}
+			// restStrListの取得(自身の関数にtextを投入)
+			restStrList = makeRomaTextList(resttext, idb==CHAR_DATA ? MOVE_DATA : CHAR_DATA);
+
+			// baseStrListとrestStrLisrの組合せ
+			int baseN, restN;
+			baseN = baseStrList.size() > 0 ? baseStrList.size() : 1;
+			restN = restStrList.size() > 0 ? restStrList.size() : 1;
+			for(j=0;j<baseN;j++){
+				for(k=0;k<restN;k++){
+					String addText =  baseStrList.size() > 0 ? baseStrList(j) : "";
+					String addText += restStrList.size() > 0 ? restStrList(k) : "";
+					if(!addText.equals("")) rtn.add(addText);
+				}
+			}
+		}
+		return rtn;
 	}
 
+	////////////////////////////////////////////////////////////////////////////////
 	// public method
 	public void append(char str){
 		mSlipText.append(str);
